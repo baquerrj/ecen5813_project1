9ad543c025da81d49e2bc62d9b80b3ded2ed1123
Roberto Baquerizo <roba8460@colorado.edu>
2018-09-23 15:24:43 -0600

    Added \r, but thinks everything is an unrecognized command.|

--- src/main.c
+++ src/main.c
@@ -9,9 +9,10 @@ int main( void )
    char* p_input = (char*) calloc( MAX_INPUT_LENGTH, sizeof( char* ) );
    uint8_t firstTime = 0;
    uint8_t allocated = 0;
+   void* mem;
    if( 0 == firstTime )
    {
-      printf( "Hello and welcome! Enter 'help' for help menu\n" );
+      printf( "Hello and welcome! Enter 'help' for help menu\n\r" );
       firstTime++;
    }
 
@@ -21,12 +22,12 @@ int main( void )
 
       if( fgets( p_input, MAX_INPUT_LENGTH, stdin ) != NULL )
       {
-         if( 0 == strcmp( "help\n", p_input ) )
+         if( 0 == strcmp( "help\n\r", p_input ) )
          {
             help();
             continue;
          }
-         else if( 0 == strcmp( "quit\n", p_input ) )
+         else if( 0 == strcmp( "quit\n\r", p_input ) )
          {
             if( 1 == allocated )
             {
@@ -38,23 +39,23 @@ int main( void )
             printf( "Exiting...");
             break;
          }
-         else if( ( 0 == allocated ) && ( 0 == strcmp( "allocate\n", p_input ) ) )
+         else if( ( 0 == allocated ) && ( 0 == strcmp( "allocate\n\r", p_input ) ) )
          {
             uint32_t nWords = getNumberOfWords();
             mem = allocate( nWords );
             allocated = 1;
             continue;
          }
-         else if( ( 1 == allocated ) && ( 0 == strcmp( "free\n", p_input ) ) )
+         else if( ( 1 == allocated ) && ( 0 == strcmp( "free\n\r", p_input ) ) )
          {
-            printf( "Freeing %u byes of memory\n", size );
+            printf( "Freeing %u byes of memory\n\r", size );
             free( mem );
             allocated = 0;
             continue;
          }
          else
          {
-            printf( "Unrecognized command!\n" );
+            printf( "Unrecognized command!\n\r" );
             continue;
          }
       }
@@ -70,7 +71,7 @@ void* allocate( uint32_t nWords )
    size = nWords * sizeof( uint32_t );
    mem = malloc( size );
 
-   printf( "Allocated %u bytes of memory for %d 32-bit words at adress %p.\n",
+   printf( "Allocated %u bytes of memory for %d 32-bit words at adress %p.\n\r",
                size, nWords, mem );
 
    return mem;
@@ -79,21 +80,21 @@ void* allocate( uint32_t nWords )
 
 void help( void )
 {
-   printf( "Hello and welcome!\n" );
-   printf( "This utility allows the user to allocate memory\n" );
-   printf( "for a specified number of 32-bit words, and do all\n" );
-   printf( "sorts of cool stuff with the allocated memory.\n\n" );
-   printf( "For example, you can request to see the contents of a memory address\n" );
-   printf( "you specify, and the number of 32-bit words to display.\n" );
-   printf( "You can also write a non-negative value to a memory address.\n" );
-   printf( "If you just want to mess with the allocated memory, for\n" );
-   printf( "whatever reason, you can request to invert all memory bits in a\n" );
-   printf( "specified area of memory, defined by address and number of 32-bit words.\n" );
-   printf( "If you are not creative enough to think of a value to write to memory,\n" );
-   printf( "I can write a random pattern to an area of memory you specify, but you\n" );
-   printf( "must at least be creative enough to provide a seed value for me to work with.\n" );
-   printf( "If you choose to go the randomly-generated pattern route, and you would then\n" );
-   printf( "want to verify the pattern written to a specified area of memory, you need only ask.\n" );
+   printf( "Hello and welcome!\n\r" );
+   printf( "This utility allows the user to allocate memory\n\r" );
+   printf( "for a specified number of 32-bit words, and do all\n\r" );
+   printf( "sorts of cool stuff with the allocated memory.\n\r\n\r" );
+   printf( "For example, you can request to see the contents of a memory address\n\r" );
+   printf( "you specify, and the number of 32-bit words to display.\n\r" );
+   printf( "You can also write a non-negative value to a memory address.\n\r" );
+   printf( "If you just want to mess with the allocated memory, for\n\r" );
+   printf( "whatever reason, you can request to invert all memory bits in a\n\r" );
+   printf( "specified area of memory, defined by address and number of 32-bit words.\n\r" );
+   printf( "If you are not creative enough to think of a value to write to memory,\n\r" );
+   printf( "I can write a random pattern to an area of memory you specify, but you\n\r" );
+   printf( "must at least be creative enough to provide a seed value for me to work with.\n\r" );
+   printf( "If you choose to go the randomly-generated pattern route, and you would then\n\r" );
+   printf( "want to verify the pattern written to a specified area of memory, you need only ask.\n\r" );
    return;
 }
 
@@ -105,12 +106,12 @@ uint32_t getNumber( void )
    {
       if( ch == '-' )
       {
-         printf( "ERROR: Input cannot be a negative integer.\n" );
+         printf( "ERROR: Input cannot be a negative integer.\n\r" );
          return 0;
       }
       else if( ch == '.' )
       {
-         printf( "ERROR: Input must be an integer, not a double.\n" );
+         printf( "ERROR: Input must be an integer, not a double.\n\r" );
          return 0;
       }
       if( '9' < ch || '0' > ch )
@@ -125,16 +126,16 @@ uint32_t getNumber( void )
 
 void writeToMemory( void *p_address, uint32_t val )
 {
-   printf( "Writing value of %x to memory address\n", val );
+   printf( "Writing value of %x to memory address\n\r", val );
    return;
 }
 
 uint32_t getNumberOfWords( void )
 {
    uint32_t nWords = 0;
-   printf( "How many 32-bit words do you wish to allocate memory for?\n ");
+   printf( "How many 32-bit words do you wish to allocate memory for?\n\r");
    nWords = getNumber();
-   printf( "%u words it is!\n", nWords );
+   printf( "%u words it is!\n\r", nWords );
 
    return nWords;
 }

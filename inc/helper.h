2cc27fbed72295525710c58cb4335d992da06d0f
Roberto Baquerizo <roba8460@colorado.edu>
2018-09-24 16:42:13 -0600

    Added descriptions to parseHex(), doAllocate, notHex, size, and modified description for getNumber()

--- inc/helper.h
+++ inc/helper.h
@@ -3,14 +3,31 @@
 
 #define MAX_INPUT_LENGTH 20
 
+// Size of memory to allocate
+// Calculated in allocate().
 uint32_t size = 0;
+
+/* Signal if input to stdin is in hex format
+   0: input is in hex format (false)
+   1: input is not in hex format (true) */
 uint8_t notHex = 0;
+
+/* Signal if we should allocate memory:
+   0: prevents allocation
+      User input to getNumber() from stdin was:
+        1) negative integer.
+        2) not a number, e.g. a letter.
+        3) not an integer, e.g. a float/double.
+   1: allows allocation */
+uint8_t doAllocate = 1;
+
 // Print help menu
 void help( void );
 
-uint32_t parseHex( void );
+// Parse input hex value to get integer value
+uint32_t parseHex( char* p_hex );
 
-// Get unsigned integer from stdin
+// Get integer value from stdin
 uint32_t getNumber( void );
 
 // Write usigned integer vall to memory address

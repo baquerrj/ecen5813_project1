81c28149d2e96092d760c8583838314ed86c38c5
Roberto Baquerizo <roba8460@colorado.edu>
2018-09-25 20:13:22 -0600

    Added comments and conformed them to coding standard

--- inc/helper.h
+++ inc/helper.h
@@ -3,8 +3,8 @@
 
 #define MAX_INPUT_LENGTH 20
 
-// Size of memory to allocate
-// Calculated in allocate().
+/* Size of memory to allocate.
+ * calculated in allocate() */
 uint32_t size = 0;
 
 /* Signal if input to stdin is in hex format
@@ -13,32 +13,32 @@ uint32_t size = 0;
 uint8_t notHex = 0;
 
 /* Signal if we should allocate memory:
-   0: prevents allocation
-      User input to getNumber() from stdin was:
-        1) negative integer.
-        2) not a number, e.g. a letter.
-        3) not an integer, e.g. a float/double.
-   1: allows allocation */
+ * 0: prevents allocation
+ *    User input to getNumber() from stdin was:
+ *      1) negative integer.
+ *      2) not a number, e.g. a letter.
+ *      3) not an integer, e.g. a float/double.
+ * 1: allows allocation */
 uint8_t doAllocate = 1;
 
-// Print help menu
+/* Print help menu */
 void help( void );
 
-// Parse input hex value to get integer value
+/* Parse input hex value and returns uint32_t value */
 uint32_t parseHex( char* p_hex );
 
-// Get integer value from stdin
+/* Parse input from stdin and returns uint32_t value */
 uint32_t getNumber( void );
 
-// Write usigned integer vall to memory address
-// pointed to by p_address
+/* Write uint32_t val to memory address
+ * pointed to by p_address */
 void writeToMemory( void *p_address, uint32_t val );
 
-// Get number of 32-bit bit words. Calls getNumber()
+/* Genumber of words to allocate memory for
+ * Calls getNumber() to read fro stdin */
 uint32_t getNumberOfWords( void );
 
-// Allocate memory for specified number
-// 32-bit words. Returns pointer to memory location
+/* Allocate memory for specified number Returns pointer to memory location */
 void* allocate( uint32_t nWords );
 
 #endif /* HELPER_H */

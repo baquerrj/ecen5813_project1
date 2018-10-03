6b33535f4302c3ea116d091e9bad26543dcf92b7
Roberto Baquerizo <roba8460@colorado.edu>
2018-10-02 19:10:39 -0600

    Erased some whitspace

--- inc/writeToMemory.h
+++ inc/writeToMemory.h
@@ -5,11 +5,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 /* Write uint32_t val to memory address
  * pointed to by p_address */
 void writeToMemory( void *p_address, uint32_t val );
-
-
 
 
 #endif /* WRITE_TO_MEMORY_H */

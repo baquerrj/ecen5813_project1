6c14faae0a76d989f1cee3a6a78b16e39c64ae27
Roberto Baquerizo <roba8460@colorado.edu>
2018-10-02 19:49:23 -0600

    Check if malloc returns NULL*. If so print error

--- src/allocate.c
+++ src/allocate.c
@@ -10,9 +10,16 @@ uint32_t allocate( void **mem, uint32_t nWords )
 {
    uint32_t size = nWords * sizeof( uint32_t );
    *mem = malloc( size );
-
-   printf( "Allocated %u bytes of memory for %d 32-bit words at adress %p.\n\r",
-            size, nWords, *mem );
+   
+   if( NULL == *mem )
+   {
+      printf( "ERROR: Could not allocate memory!\n\r" );
+   }
+   else
+   {
+      printf( "Allocated %u bytes of memory for %d 32-bit words at adress %p.\n\r",
+               size, nWords, *mem );
+   }
 
    return size;
 }

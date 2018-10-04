d9c3d31446f1c8bb77e015b9596e69bde57e612c
Roberto Baquerizo <roba8460@colorado.edu>
2018-10-03 18:25:20 -0600

    removed uneeded includes

--- inc/getNumberOfWords.h
+++ inc/getNumberOfWords.h
@@ -1,28 +1,9 @@
 #ifndef GET_NUMBER_OF_WORDS_H
 #define GET_NUMBER_OF_WORDS_H
 #include <stdint.h>
-#include <inttypes.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* Genumber of words to allocate memory for
  * Calls getNumber() to read fro stdin */
 uint32_t getNumberOfWords( void );
-
-
 
 #endif /* GET_NUMBER_OF_WORDS_H */
